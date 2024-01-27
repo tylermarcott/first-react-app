e85932baddc15f9dfa830299b13b861bf252c88f
Tyler <tylerjmarcott@gmail.com>
2024-01-27 14:09:29 -0700

    notes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 
 function App() {
   return (
+
+    // NOTE: code below is not HTML, it is JSX. JSX allows us to write HTML in React, inside a JS file
+    // differences: classname, not class
+    // in curly braces, we can write any JS expression. eg: src={logo}  <---- dynamically adding the link to logo for the image
+    // can also do <p> {2 + 2} </p> or something like this.
+
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// starting point of every react.js application
+
+// @ts-ignore
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
