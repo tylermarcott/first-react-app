ffab4baa6d34c4d60ef0155af2c471be59600ca9
Tyler <tylerjmarcott@gmail.com>
2024-01-27 14:13:15 -0700

    added intellisense for jsx

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import React from 'react';
 import './App.css';
 
@@ -9,23 +8,9 @@ function App() {
     // NOTE: code below is not HTML, it is JSX. JSX allows us to write HTML in React, inside a JS file
     // differences: classname, not class
     // in curly braces, we can write any JS expression. eg: src={logo}  <---- dynamically adding the link to logo for the image
-    // can also do <p> {2 + 2} </p> or something like this.
+    // can also do <p> {2 + 2} </p> or something like this. This will render 4 on the page
 
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
